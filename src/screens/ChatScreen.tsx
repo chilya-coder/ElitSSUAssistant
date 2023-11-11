65916b6e990f8e719dce1f5df7a476b7f546fb02
chilya-coder <yuliya.chimyrys@gmail.com>
2023-11-11 21:21:39 +0000

    [ELITChat] Chat UI enhanced, new style and animations added. SSUElitAPI integrated.

--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -7,7 +7,6 @@ import {
   Image,
 } from "react-native";
 import React, { useEffect, useState } from "react";
-import { dummyMessages } from "./dummyMessages";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
@@ -15,53 +14,133 @@ import {
 
 import { AutoGrowingTextInput } from "react-native-autogrow-textinput";
 import { SSUElitAPI } from "../services/api/SSUElitApi";
+import LottieView from "lottie-react-native";
+import ElitSSUAssistantStrings from "../ElitSSUAssistantStrings.json";
 
 export default function ChatScreen() {
-  const [messages, setMessages] = useState(dummyMessages);
+  const [messages, setMessages] = useState([]);
   const [userMessage, setUserMessage] = useState("");
   const [userPrompt, setUserPrompt] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const api = new SSUElitAPI();
+
   function handleOnChangeText(text: string) {
     setUserMessage(text);
   }
 
   useEffect(() => {
-    if (userPrompt !== "") {
-      api.sendAIRequest({ prompt: userPrompt }).then((datatest) => {
-        console.log("API response: ", datatest);
-        setMessages([...messages, { role: "bot", content: datatest.body }]);
-        setUserPrompt("");
-      });
+    if (userPrompt !== "" && isLoading) {
+      setMessages([
+        ...messages,
+        { role: "bot", content: ElitSSUAssistantStrings.loading_message },
+      ]);
+      api
+        .sendAIRequest({ prompt: userPrompt })
+        .then((datatest) => {
+          setIsLoading(false);
+          setMessages([...messages, { role: "bot", content: datatest.body }]);
+          console.log("AI API response: ", datatest.body);
+          setUserPrompt("");
+        })
+        .catch((error) => {
+          setMessages([
+            ...messages,
+            {
+              role: "bot",
+              content: ElitSSUAssistantStrings.internal_server_error,
+            },
+          ]);
+          setIsLoading(false);
+        });
     }
   }, [userPrompt]);
 
   function handleOnPressButton() {
-    console.log("Button pressed!");
-    setUserMessage("");
+    console.log("User input", userMessage);
+    if (!/^[a-zA-Zа-яА-ЯёЁіІїЇєЄ\s\p{P}]+$/u.test(userMessage)) {
+      return;
+    }
     setUserPrompt(userMessage);
+    setIsLoading(true);
+    setUserMessage("");
     setMessages([...messages, { role: "user", content: userMessage }]);
   }
-
+  console.log("Check it", messages);
   return (
-    <SafeAreaView className="flex-1 py-10 justify-center bg-white">
-      <View className="flex-row mx-5 mt-14 mb-5 justify-center">
+    <SafeAreaView className="flex-1 pb-4 justify-center bg-white">
+      <View
+        style={{
+          backgroundColor: "#F5F5F5",
+          padding: 25,
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+          className="mt-5"
+        >
+          <TouchableOpacity>
+            <Text
+              style={{ fontSize: 16, fontWeight: "bold", textAlign: "left" }}
+            >
+              Hello
+            </Text>
+          </TouchableOpacity>
+          <Text
+            style={{ fontSize: 16, fontWeight: "bold", textAlign: "right" }}
+          >
+            ELIT Bot
+          </Text>
+        </View>
+      </View>
+
+      {
         <ScrollView
           bounces={false}
-          className="space-y-5"
+          className="space-y-5 mx-5 mt-3 mb-3"
           showsVerticalScrollIndicator={false}
+          // style={{ backgroundColor: "#7FFFD4" }}
         >
+          {messages.length === 0 && (
+            <View className="pt-40 pl-5" style={{ alignItems: "center" }}>
+              <LottieView
+                style={{
+                  height: 150,
+                  alignContent: "center",
+                }}
+                source={require("../../assets/images/lottie/welcome_bot.json")}
+                autoPlay
+              />
+              <Text
+                className="mt-3"
+                style={{ fontSize: 16, fontWeight: "bold" }}
+              >
+                Hi! Ask me anything about ELIT.
+              </Text>
+            </View>
+          )}
           {messages.map((message, index) => {
-            if (message.role === "bot") {
+            if (message.role === "user") {
               return (
-                <View key={index} className="flex row justify-start pr-8">
+                <View key={index} className="flex row justify-end pl-8 pr-1">
                   <View
                     style={{
-                      backgroundColor: "#cffafe",
+                      backgroundColor: "#F5F5F5",
                       borderRadius: 10,
                       padding: 10,
+                      // shadowColor: "#000",
+                      // shadowOffset: {
+                      //   width: 1,
+                      //   height: 1,
+                      // },
+                      // shadowOpacity: 0.22,
+                      // shadowRadius: 2.22,
+                      // elevation: 3,
                     }}
-                    className="rounded-3xl p-4 rounded-tl-none"
+                    className="rounded-3xl p-4 rounded-tr-none"
                   >
                     <Text>{message.content}</Text>
                   </View>
@@ -69,45 +148,85 @@ export default function ChatScreen() {
               );
             } else {
               return (
-                <View key={index} className="flex row justify-end pl-8">
+                <View key={index} className="flex row justify-start pr-8 pl-1">
                   <View
                     style={{
-                      backgroundColor: "#F5F5F5",
+                      backgroundColor: "#bfdbfe",
                       borderRadius: 10,
                       padding: 10,
                     }}
-                    className="bg-white rounded-3xl p-4 rounded-tr-none"
+                    className="rounded-3xl p-2 rounded-tl-none"
                   >
-                    <Text>{message.content}</Text>
+                    <Text>
+                      <Text>
+                        {message.content ===
+                        ElitSSUAssistantStrings.loading_message ? (
+                          <LottieView
+                            style={{
+                              height: 45,
+                            }}
+                            source={require("../../assets/images/lottie/loading.json")}
+                            autoPlay
+                          />
+                        ) : (
+                          message.content
+                        )}
+                      </Text>
+                    </Text>
                   </View>
                 </View>
               );
             }
           })}
         </ScrollView>
-      </View>
-      <View className="flex-row justify-center items-center mb-3.5">
-        <View className="pr-14">
-          <AutoGrowingTextInput
-            value={userMessage}
-            onChangeText={handleOnChangeText}
-            multiline={true}
-            className="rounded-3xl p-4"
-            style={{
-              backgroundColor: "#F5F5F5",
-              width: wp(80),
-              height: hp(7.5),
-            }}
-          ></AutoGrowingTextInput>
-        </View>
+      }
+      <View>
+        <AutoGrowingTextInput
+          value={userMessage}
+          placeholder={ElitSSUAssistantStrings.send_message_placeholder}
+          onChangeText={handleOnChangeText}
+          placeholderTextColor={"grey"}
+          multiline={true}
+          className="rounded-3xl p-3 left-4 bg-blue-50"
+          style={{
+            // backgroundColor: "#F5F5F5",
+            width: wp(75),
+            height: hp(7.5),
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 1,
+            },
+            shadowOpacity: 0.2,
+            shadowRadius: 1.41,
+
+            elevation: 2,
+          }}
+        ></AutoGrowingTextInput>
         <TouchableOpacity
-          className={"bg-cyan-50 rounded-3xl p-3 absolute right-3"}
-          style={{ width: wp(12), height: hp(6) }}
+          className={"rounded-3xl p-3 absolute right-4 bg-blue-50"}
+          style={{
+            //backgroundColor: "#F5F5F5",
+            width: wp(14),
+            height: hp(7),
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 1,
+            },
+            shadowOpacity: 0.4,
+            shadowRadius: 1.41,
+            elevation: 2,
+          }}
           onPress={handleOnPressButton}
         >
           <Image
+            className="mx-1 my-1"
             source={require("../../assets/images/sendicon.png")}
-            style={{ height: hp(3), width: hp(3) }}
+            style={{
+              height: hp(3),
+              width: hp(3),
+            }}
           ></Image>
         </TouchableOpacity>
       </View>
--- src/services/api/SSUElitAPI.tsx
+++ src/services/api/SSUElitAPI.tsx
@@ -1,12 +1,12 @@
+import ElitSSUAssistantStrings from "../../ElitSSUAssistantStrings.json";
+
 interface HumanPrompt {
   prompt: string;
 }
 
-const serverAddress = process.env.SERVER_ADDRESS;
-
 export class SSUElitAPI {
   async sendAIRequest(properties: HumanPrompt) {
-    return fetch(`http://${serverAddress}:8080/chatbot`, {
+    return fetch(`ip_address/chatbot`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ prompt: properties.prompt }),
@@ -17,6 +17,8 @@ export class SSUElitAPI {
         }
         return await response.json();
       })
-      .catch((error) => console.error("This is error", error));
+      .catch((error) => {
+        console.error(ElitSSUAssistantStrings.request_failed, error);
+      });
   }
 }
