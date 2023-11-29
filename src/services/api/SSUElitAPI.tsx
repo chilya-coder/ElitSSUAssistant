e08e147a68670a38325dc59f1e755b1998f195cb
chilya-coder <yuliya.chimyrys@gmail.com>
2023-11-29 17:23:24 +0000

    Update language parameter in API request

--- src/services/api/SSUElitAPI.tsx
+++ src/services/api/SSUElitAPI.tsx
@@ -2,14 +2,29 @@ import ElitSSUAssistantStrings from "../../ElitSSUAssistantStrings.json";
 
 interface HumanPrompt {
   prompt: string;
+  language: string;
+}
+
+function formatLanguage(language: string) {
+  switch (language) {
+    case "en":
+      return "English";
+    case "ua":
+      return "Ukrainian";
+    default:
+      return "Ukrainian";
+  }
 }
 
 export class SSUElitAPI {
   async sendAIRequest(properties: HumanPrompt) {
-    return fetch(`ip_address/chatbot`, {
+    return fetch(process.env.EXPO_PUBLIC_SSU_API_URL + "/chatbot", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ prompt: properties.prompt }),
+      body: JSON.stringify({
+        prompt: properties.prompt,
+        language: formatLanguage(properties.language),
+      }),
     })
       .then(async (response) => {
         if (!response.ok) {
